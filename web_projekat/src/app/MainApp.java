214987d95713847cae9963638e8f81ca64335c94
Aleksandar Mijatovic <acamijatovic.98@gmail.com>
2020-09-09 17:40:02 +0200

    proba

--- web_projekat/src/app/MainApp.java
+++ web_projekat/src/app/MainApp.java
@@ -1,10 +1,23 @@
 package app;
 
+import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.staticFiles;
+
+import java.io.File;
 public class MainApp {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception{
+				port(41);
+				staticFiles.externalLocation(new File("./static").getCanonicalPath());
 
-	}
+			
+				
+				System.out.println("asdad");
+			}
 
-}
+		}
+
+	
+
+
